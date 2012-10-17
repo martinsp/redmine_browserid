10971edcacde93c7e0e9820dc79c3404ae65a8ba
Martins Polakovs <martins.polakovs@gmail.com>
2012-10-17 19:41:19 +0300

    Dump version to 0.1

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :redmine_browserid do
   name 'Redmine Mozilla Persona/BrowserID plugin'
   author 'Martins Polakovs'
   description 'This plugin adds Mozilla Persona/BrowserID authentication to redmine'
-  version '0.0.1'
+  version '0.1'
   url 'https://github.com/martinsp/redmine_browserid'
   author_url 'https://github.com/martinsp'
 end
